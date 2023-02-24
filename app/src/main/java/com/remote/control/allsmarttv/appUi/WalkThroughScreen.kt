8fb015e55423afe2cdfbb48a52d28c273b63b2af
mraheeleng <57173923+mraheel48@users.noreply.github.com>
2023-02-24 19:47:06 +0500

    update_new_design_Build_vesion_41

---
+++ app/src/main/java/com/remote/control/allsmarttv/appUi/WalkThroughScreen.kt
@@ -0,0 +1,11 @@
+package com.remote.control.allsmarttv.appUi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalkThroughScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk_through_screen)
+    }
+}
